164dcf37d1320d52e8aeb679ed32705cf96d9e88
Alexander Zarubin <nonexist.void@gmail.com>
2019-06-05 14:01:47 +0300

    shared expectations

---
+++ tests/server/mocks/expected-data.js
@@ -0,0 +1,115 @@
+module.exports = {
+  rules: [
+    {
+      script: 'rule1 script',
+      name: 'rule1',
+      order: 10,
+      stage: 'login_success',
+      enabled: false
+    },
+    {
+      enabled: undefined,
+      script: 'rule2 script',
+      name: 'rule2',
+      order: 0,
+      stage: 'login_success'
+    }
+  ],
+  databases: [
+    {
+      enabled_clients: [
+        'client'
+      ],
+      options: {
+        customScripts: {
+          login: 'login function content'
+        },
+        enabledDatabaseCustomization: true
+      },
+      strategy: 'auth0',
+      name: 'test-db'
+    }
+  ],
+  emailProvider: {
+    name: 'smtp',
+    enabled: true,
+    credentials: {
+      smtp_host: 'smtp.server.com',
+      smtp_port: 25,
+      smtp_user: 'smtp_user',
+      smtp_pass: 'smtp_secret_password'
+    }
+  },
+  emailTemplates: [
+    {
+      template: 'blocked_account',
+      from: 'from',
+      subject: 'subject',
+      resultUrl: 'url',
+      syntax: 'liquid',
+      body: 'blocked email html',
+      urlLifetimeInSeconds: 432000,
+      enabled: true
+    }
+  ],
+  pages: [
+    {
+      enabled: undefined,
+      html: 'login page html',
+      name: 'login'
+    }
+  ],
+  roles: [
+    {
+      name: 'Test role',
+      description: 'test',
+      permissions: [
+        {
+          permission_name: 'update:account',
+          resource_server_identifier: 'sapi'
+        }
+      ]
+    }
+  ],
+  clients: [
+    {
+      name: 'client'
+    }
+  ],
+  clientGrants: [
+    {
+      client_id: 'client',
+      audience: 'sapi',
+      scope: [
+        'update:account'
+      ]
+    }
+  ],
+  connections: [
+    {
+      name: 'connection'
+    }
+  ],
+  rulesConfigs: [
+    {
+      key: 'secret1',
+      value: 'first-secret'
+    },
+    {
+      key: 'secret2',
+      value: 'second-secret'
+    }
+  ],
+  resourceServers: [
+    {
+      name: 'some-api',
+      identifier: 'sapi',
+      scopes: [
+        {
+          description: 'update account',
+          value: 'update:account'
+        }
+      ]
+    }
+  ]
+};
--- tests/server/providers/gitlab.tests.js
+++ tests/server/providers/gitlab.tests.js
@@ -4,6 +4,7 @@ import expect from 'expect';
 import config from '../../../server/lib/config';
 import { hasChanges, getChanges } from '../../../server/lib/providers/gitlab';
 import files from '../mocks/repo-tree-mock';
+import expectedResults from '../mocks/expected-data';
 
 const defaultConfig = {
   REPOSITORY: 'test/auth0',
@@ -115,78 +116,7 @@ describe('gitlab', () => {
 
       getChanges({ projectId: 'projectId', branch: 'branch' })
         .then(results => {
-          // rules
-          expect(results.rules).toBeAn('array');
-          expect(results.rules[0].name).toEqual('rule1');
-          expect(results.rules[0].script).toEqual('rule1 script');
-          expect(results.rules[0].stage).toEqual('login_success');
-          expect(results.rules[0].enabled).toEqual(false);
-          expect(results.rules[0].order).toEqual(10);
-          expect(results.rules[1].name).toEqual('rule2');
-          expect(results.rules[1].script).toEqual('rule2 script');
-          expect(results.rules[1].stage).toEqual('login_success');
-          expect(results.rules[1].order).toEqual(0);
-
-          // databases
-          expect(results.databases).toBeAn('array');
-          expect(results.databases[0].name).toEqual('test-db');
-          expect(results.databases[0].strategy).toEqual('auth0');
-          expect(results.databases[0].enabled_clients).toEqual([ 'client' ]);
-          expect(results.databases[0].options.enabledDatabaseCustomization).toEqual(true);
-          expect(results.databases[0].options.customScripts.login).toEqual('login function content');
-
-          // connections
-          expect(results.connections).toBeAn('array');
-          expect(results.connections[0].name).toEqual('connection');
-
-          // emails
-          expect(results.emailProvider).toBeAn('object');
-          expect(results.emailTemplates).toBeAn('array');
-          expect(results.emailProvider).toEqual(files.emails['provider.json']);
-          expect(results.emailTemplates[0].template).toEqual('blocked_account');
-          expect(results.emailTemplates[0].from).toEqual('from');
-          expect(results.emailTemplates[0].subject).toEqual('subject');
-          expect(results.emailTemplates[0].resultUrl).toEqual('url');
-          expect(results.emailTemplates[0].syntax).toEqual('liquid');
-          expect(results.emailTemplates[0].body).toEqual('blocked email html');
-          expect(results.emailTemplates[0].urlLifetimeInSeconds).toEqual(432000);
-          expect(results.emailTemplates[0].enabled).toEqual(true);
-
-          // pages
-          expect(results.pages).toBeAn('array');
-          expect(results.pages[0].name).toEqual('login');
-          expect(results.pages[0].html).toEqual('login page html');
-
-          // roles
-          expect(results.roles).toBeAn('array');
-          expect(results.roles[0].name).toEqual('Test role');
-          expect(results.roles[0].description).toEqual('test');
-          expect(results.roles[0].permissions[0].permission_name).toEqual('update:account');
-          expect(results.roles[0].permissions[0].resource_server_identifier).toEqual('sapi');
-
-          // clients
-          expect(results.clients).toBeAn('array');
-          expect(results.clients[0].name).toEqual('client');
-
-          // clientGrants
-          expect(results.clientGrants).toBeAn('array');
-          expect(results.clientGrants[0].client_id).toEqual('client');
-          expect(results.clientGrants[0].audience).toEqual('sapi');
-          expect(results.clientGrants[0].scope).toEqual([ 'update:account' ]);
-
-          // rulesConfigs
-          expect(results.rulesConfigs).toBeAn('array');
-          expect(results.rulesConfigs[0].key).toEqual('secret1');
-          expect(results.rulesConfigs[0].value).toEqual('first-secret');
-          expect(results.rulesConfigs[1].key).toEqual('secret2');
-          expect(results.rulesConfigs[1].value).toEqual('second-secret');
-
-          // resourceServers
-          expect(results.resourceServers).toBeAn('array');
-          expect(results.resourceServers[0].name).toEqual('some-api');
-          expect(results.resourceServers[0].identifier).toEqual('sapi');
-          expect(results.resourceServers[0].scopes[0].value).toEqual('update:account');
-          expect(results.resourceServers[0].scopes[0].description).toEqual('update account');
+          expect(results).toEqual(expectedResults);
 
           done();
         })
