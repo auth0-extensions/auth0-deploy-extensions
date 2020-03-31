b41f3ed5220a1085a1183e9d67170d5ec1286202
Shawn Mclean <shawnrmclean@gmail.com>
2020-03-31 16:18:21 -0500

    Revert "Move auth0 client from middleware to own instance."
    This reverts commit 850f968cc871f26fc1b7352f3d4ea8a14e991c21.

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -1,25 +1,25 @@
 import express from 'express';
-import { ManagementClient } from 'auth0';
+import { middlewares } from 'auth0-extension-express-tools';
 
 import config from '../lib/config';
 import deploy from '../lib/deploy';
 
 const middleware = require(`../lib/middlewares/${process.env.A0EXT_PROVIDER}`);
 
-export default storage => {
-  const auth0Client = new ManagementClient({
+export default (storage) => {
+  const webhooks = express.Router(); // eslint-disable-line new-cap
+  webhooks.use(middlewares.managementApiClient({
     domain: config('AUTH0_DOMAIN'),
     clientId: config('AUTH0_CLIENT_ID'),
     clientSecret: config('AUTH0_CLIENT_SECRET')
-  });
-  const webhooks = express.Router(); // eslint-disable-line new-cap
+  }));
 
   webhooks.post('/deploy/:secret?', middleware(), (req, res) => {
     // Send response ASAP to prevent extra requests.
     res.status(202).json({ message: 'Request accepted, deployment started.' });
 
     // Deploy the changes.
-    return deploy(storage, auth0Client, req.webhook);
+    return deploy(storage, req.auth0, req.webhook);
   });
 
   return webhooks;
